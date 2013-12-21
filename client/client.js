e298d85d8229d7226954157909156f04eb12353a
Johan Östling <johan.f.ostling@gmail.com>
2013-12-21 08:18:48 +0100

    After trial run

--- client/client.js
+++ client/client.js
@@ -1,14 +1,2 @@
 Meteor.startup(function () {
-	setPage(1);
-
-	var presentation = Meteor.render(function () {
-		var page = "" + Session.get("page");
-		return Template[page]();
-	});
-
-	document.body.appendChild(presentation);
 });
-
-window.setPage = function(page) { 
-	Session.set("page", page);
-}
